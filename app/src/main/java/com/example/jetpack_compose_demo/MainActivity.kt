876a78f2dbaabca17677889737d7abe08b147e62
jialei qin <jialei.qin@thoughtworks.com>
2022-03-01 11:17:51 +0800

    init

--- app/src/main/java/com/example/jetpack_compose_demo/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_demo/MainActivity.kt
@@ -3,31 +3,70 @@ package com.example.jetpack_compose_demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose_demo.ui.theme.JetpackcomposedemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "hello")
+            App()
+        }
+    }
+}
+
+
+@Composable
+fun App() {
+    TodoList(mutableListOf("摸鱼", "打游戏"))
+}
+
+@Composable
+fun TodoList(lists: MutableList<String>) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp)
+        ) {
+            for (list in lists) {
+                TodoListItem(text = list)
+            }
         }
     }
 }
 
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "hello $name ")
+fun TodoListItem(text: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        val done = remember { mutableStateOf(false) }
+        Text(text = if (done.value) "$text completed!" else text, Modifier.padding(5.dp))
+        OutlinedButton(onClick = { done.value = !done.value }) {
+            Text(text = "completed!")
+        }
+    }
 }
 
 @Preview
 @Composable
 fun PreviewDemo() {
-    MessageCard(name = "world22")
+    App()
 }
--- app/src/main/java/com/example/jetpack_compose_demo/function.kt
+++
@@ -1,8 +0,0 @@
-package com.example.jetpack_compose_demo
-
-
-fun double(i: Int):Int {
-    return 2 * i
-}
-
-val result = double(2)
