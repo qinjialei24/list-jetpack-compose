387f7c3829c96d83377995a1954967781fbd08ef
jialei qin <jialei.qin@thoughtworks.com>
2022-03-01 15:52:43 +0800

    feat add rounded image

--- app/src/main/java/com/example/jetpack_compose_demo/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_demo/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.jetpack_compose_demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -47,7 +52,9 @@ fun App() {
 fun GoodsList(goodsList: List<GoodsItem>) {
     Surface(
         color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp).fillMaxWidth()
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .fillMaxWidth()
     ) {
         LazyColumn() {
             items(items = goodsList) { item ->
@@ -61,7 +68,18 @@ fun GoodsList(goodsList: List<GoodsItem>) {
 @Composable
 fun GoodsListItem(item: GoodsItem) {
     val (id, name) = item
-    Row(horizontalArrangement = Arrangement.Center) {
+    Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painterResource(
+                id = R.drawable.goods1
+            ),
+            contentScale = ContentScale.Crop,
+            contentDescription = null,
+            modifier = Modifier
+                .width(100.dp)
+                .height(100.dp)
+                .clip(RoundedCornerShape(50)),
+        )
         Text(name)
     }
 }
